a2f9f9c9802122713045b0b90b71ec8c2df355b2
Serezha <serezhaolshan@gmail.com>
2020-10-14 12:55:15 +0300

    some unessecary fixes

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -8,20 +8,19 @@ int main()
 
 
 
-	char sc1[2], sc2[2], sc3[2];
-	int relMass1;
-	double relMass2, relMass3;
-	double procPart1, procPart2, procPart3;
-	unsigned long numbOfStars1, numbOfStars2, numbOfStars3;
+	char sc1, sc2, sc3;
+	double relMass1, relMass2, relMass3;
+	long double procPart1, procPart2, procPart3;
+	long long int numbOfStars1, numbOfStars2, numbOfStars3;
 
 	printf("1. Введите: спектральный класс, Приблизительную массу(отн.Солнца), Часть, Численность:");
-	scanf_s("%s %d %lf %lu", sc1, &relMass1, &procPart1, &numbOfStars1);
+	scanf_s("%c %lf %Lf %lli", &sc1, 2, &relMass1, &procPart1, &numbOfStars1);
 
 	printf("2. Введите: спектральный класс, Приблизительную массу(отн.Солнца), Часть, Численность:");
-	scanf_s("%s %f %lf %lu", sc2, &relMass2, &procPart2, &numbOfStars2);
+	scanf_s("%c %lf %Lf %lli", &sc2, 2, &relMass2, &procPart2, &numbOfStars2);
 
 	printf("3. Введите: спектральный класс, Приблизительную массу(отн.Солнца), Часть, Численность:");
-	scanf_s("%s %f %lf %lu", sc3, &relMass3, &procPart3, &numbOfStars3);
+	scanf_s("%c %lf %Lf %lli", &sc3, 2, &relMass3, &procPart3, &numbOfStars3);
 
 	printf("---------------------------------------------------------------------------\t\n");
 	printf("|Приблизительное количество звезд разных спектральных классов в Галактике |\t\n");
@@ -29,13 +28,13 @@ int main()
 	printf("| Спектральный класс |  Приблизительная масса  | Часть |    Численность   |\t\n");
 	printf("|                    |       (отн.Солнца)      |       |                  |\t\n");
 	printf("|--------------------|-------------------------|-------|------------------|\t\n");
-	printf("|       %s           |           %d            |  %lf  |       %lu        |\t\n,", sc1, relMass1, procPart1, numbOfStars1);
+	printf("|       %c           |           %f            |  %Lf  |       %lli        |\t\n", sc1, relMass1, procPart1, numbOfStars1);
 	printf("|--------------------|-------------------------|-------|------------------|\t\n");
-	printf("|       %s           |           %d            |  %lf  |       %lu        |\t\n,", sc2, relMass2, procPart2, numbOfStars2);
+	printf("|       %c           |           %f            |  %Lf  |       %lli        |\t\n", sc2, relMass2, procPart2, numbOfStars2);
 	printf("|--------------------|-------------------------|-------|------------------|\t\n");
-	printf("|       %s           |           %d            |  %lf  |       %lu        |\t\n,", sc3, relMass3, procPart3, numbOfStars3);
+	printf("|       %c           |           %f            |  %Lf  |       %lli        |\t\n", sc3, relMass3, procPart3, numbOfStars3);
 	printf("|-------------------------------------------------------------------------|\t\n");
 	printf("|Примечание: не показаны данные для классов: B, A, G, K                   |\t\n");
 	printf("---------------------------------------------------------------------------\t\n");
-
+	return 0;
 }
