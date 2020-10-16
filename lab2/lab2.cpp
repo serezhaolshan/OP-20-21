5cc00a8434180871722bc6b19c1fb69db9a79cd0
Serezha <serezhaolshan@gmail.com>
2020-10-16 09:54:23 +0300

    Fixed second lab

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,5 +1,7 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
+
 
 int main()
 {
@@ -7,34 +9,37 @@ int main()
 	system("cls");
 
 
-
 	char sc1, sc2, sc3;
-	double relMass1, relMass2, relMass3;
-	long double procPart1, procPart2, procPart3;
-	long long int numbOfStars1, numbOfStars2, numbOfStars3;
+	int apprWeight1, numberOfStars1;
+	double apprWeight2;
+	float apprWeight3, percent2, percent3;
+	unsigned long long int numberOfStars2, numberOfStars3;
+	long double percent1;
 
-	printf("1. Введите: спектральный класс, Приблизительную массу(отн.Солнца), Часть, Численность:");
-	scanf_s("%c %lf %Lf %lli", &sc1, 2, &relMass1, &procPart1, &numbOfStars1);
+	printf("1. Введите: спектральный класс, Приблизительную массу , Часть , Численность >");
+	scanf("%c %i %Lf %i", &sc1, &apprWeight1, &percent1, &numberOfStars1);
+	getchar();
+	printf("2. Введите: спектральный класс, Приблизительную массу , Часть , Численность >");
+	scanf("%c %lf %f %lli", &sc2, &apprWeight2, &percent2, &numberOfStars2);
+	getchar();
+	printf("3. Введите: спектральный класс, Приблизительную массу , Часть , Численность >");
+	scanf("%c %f %f %lli", &sc3, &apprWeight3, &percent3, &numberOfStars3);
+	
 
-	printf("2. Введите: спектральный класс, Приблизительную массу(отн.Солнца), Часть, Численность:");
-	scanf_s("%c %lf %Lf %lli", &sc2, 2, &relMass2, &procPart2, &numbOfStars2);
+	printf("--------------------------------------------------------------------------\n");
+	printf("|Приблизительное количество звезд разных спектральных классов в Галактике|\n");
+	printf("|------------------------------------------------------------------------|\n");
+	printf("| Спектральный класс | Приблизительная масса | Часть  |    Численность   |\n");
+	printf("|--------------------|-----------------------|--------|------------------|\n");
+	printf("| %-18c | %-21i | %-6.5Lf| %-16i |\n", sc1, apprWeight1, percent1, numberOfStars1);
+	printf("|--------------------|-----------------------|--------|------------------|\n");
+	printf("| %-18c | %-21.2lf | %-6.1f | %-16llu |\n", sc2, apprWeight2, percent2, numberOfStars2);
+	printf("|--------------------|-----------------------|--------|------------------|\n");
+	printf("| %-18c | %-21.1f | %-6.1f | %-16llu |\n", sc3, apprWeight3, percent3, numberOfStars3);
+	printf("|------------------------------------------------------------------------|\n");
+	printf("|        Примечание: не показаны данные для классов: B, A, G, K          |\n");
+	printf("--------------------------------------------------------------------------\n");
 
-	printf("3. Введите: спектральный класс, Приблизительную массу(отн.Солнца), Часть, Численность:");
-	scanf_s("%c %lf %Lf %lli", &sc3, 2, &relMass3, &procPart3, &numbOfStars3);
 
-	printf("---------------------------------------------------------------------------\t\n");
-	printf("|Приблизительное количество звезд разных спектральных классов в Галактике |\t\n");
-	printf("|-------------------------------------------------------------------------|\t\n");
-	printf("| Спектральный класс |  Приблизительная масса  | Часть |    Численность   |\t\n");
-	printf("|                    |       (отн.Солнца)      |       |                  |\t\n");
-	printf("|--------------------|-------------------------|-------|------------------|\t\n");
-	printf("|       %c           |           %f            |  %Lf  |       %lli        |\t\n", sc1, relMass1, procPart1, numbOfStars1);
-	printf("|--------------------|-------------------------|-------|------------------|\t\n");
-	printf("|       %c           |           %f            |  %Lf  |       %lli        |\t\n", sc2, relMass2, procPart2, numbOfStars2);
-	printf("|--------------------|-------------------------|-------|------------------|\t\n");
-	printf("|       %c           |           %f            |  %Lf  |       %lli        |\t\n", sc3, relMass3, procPart3, numbOfStars3);
-	printf("|-------------------------------------------------------------------------|\t\n");
-	printf("|Примечание: не показаны данные для классов: B, A, G, K                   |\t\n");
-	printf("---------------------------------------------------------------------------\t\n");
-	return 0;
 }
+
---
+++ lab3/lab3.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	double t1, t2;
+
+	t2 = (2 * b * tan((a * x) / 2)) / a * sqrt((b - c) * (b + c));
+}
+
+
