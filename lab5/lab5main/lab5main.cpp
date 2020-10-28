af2a1bef390cecc25c10738c4e894ffddffc1ab6
Serezha <serezhaolshan@gmail.com>
2020-10-28 15:29:19 +0200

    added lab5main

--- lab5/lab5main/lab5main.cpp
+++ lab5/lab5main/lab5main.cpp
@@ -7,5 +7,20 @@ int main()
 {
 	system("chcp 1251");
 	system("cls");
+
+	double x, y;
+
+	printf("Введите координату x >");
+	scanf("%lf", &x);
+	printf("Введите координату y >");
+	scanf("%lf", &y);
+
+	if ((x >= -1 && x <= 1 && y >= 0 && y <= 1) || (x <= 1 && x >= 0 && y <= 0 && y >= -1)) {
+		printf("Точка попадает в область\n");
+	}
+	else {
+		printf("Точка не попадает в область\n");
+	}
+	return 0;
 }
 
